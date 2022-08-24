9430844fd96625d43f4bf4942d1a1104c8928161
Renan Schabarum <schabarum97@gmail.com>
2022-08-23 21:43:54 -0300

    Atividades

---
+++ Ex/functions.js
@@ -0,0 +1,17 @@
+// function soma (a,b){
+//     return a + b
+// }
+
+
+// let soma = function (a,b) {
+//     return a+b    
+// }
+// let funSomar = soma(1,2)
+// funSomar *= 3
+// console.log(funSomar)
+
+const soma = (a,b) => {
+    return a+b
+
+}
+console.log(soma(1,2))
