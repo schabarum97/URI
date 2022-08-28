aaf8d9bc21fb865f7ae2b6b9a460cfe7d619a70c
Renan Schabarum <schabarum97@gmail.com>
2022-08-28 09:51:08 -0300

    Atividades

--- URI/1035.js
+++ URI/1035.js
@@ -7,5 +7,10 @@ let c = +lines.shift()
 let d = +lines.shift()
 x = c+d
 y = a+b
-if (b > c)  (d>a) && (x>y) && (c>0) && (d>0) && a % 2 == 0
-    console.log()
+if ((b > c) && (d>a) && (x>y) && (c>0) && (d>0) && (a % 2 == 0)) {
+    console.log('Valores aceitos')
+}else{
+    console.log('Valores nao aceitos')
+}
+    
+
